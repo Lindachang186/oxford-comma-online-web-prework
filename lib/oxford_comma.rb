240907f5f8e55a78465fcf622abf3120168382a0
Linda Chang <lindachang186@gmail.com>
2018-10-10 21:06:33 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,5 +6,9 @@ elsif array.size == 2
 elsif array.size == 3
   array[2] = "and starfruit"
     array.join(", ")
+elsif array.size > 3 
+  i = array.size 
+  array[i - 1] = array.join("and ")
+  array.join(" , ")
 end
 end
