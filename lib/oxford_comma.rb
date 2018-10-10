21fbc564b6bebaf0542babb100a5e2f5d919a94c
Linda Chang <lindachang186@gmail.com>
2018-10-10 21:02:10 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,9 @@
 def oxford_comma(array)
-if array.size < 1
+if array.size == 1
   return array.join
-elsif array.size < 2
+elsif array.size == 2
   return array.join("and")
-elsif array.size < 3
+elsif array.size == 3
   array.insert(2, "and")
   array.join(",")
 end
