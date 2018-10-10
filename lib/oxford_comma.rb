7bb5db44d17b8d4c537bf94a85b3e5f04af0726c
Linda Chang <lindachang186@gmail.com>
2018-10-10 21:06:36 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,8 +6,8 @@ elsif array.size == 2
 elsif array.size == 3
   array[2] = "and starfruit"
     array.join(", ")
-elsif array.size > 3 
-  i = array.size 
+elsif array.size > 3
+  i = array.size
   array[i - 1] = array.join("and ")
   array.join(" , ")
 end
