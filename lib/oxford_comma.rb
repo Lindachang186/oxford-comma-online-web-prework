21d6221871f1aeaacd12cc1887eb171cbbac5c9f
Linda Chang <lindachang186@gmail.com>
2018-10-10 21:31:45 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,7 +6,7 @@ elsif array.size == 2
 elsif array.size == 3
   array[2] = "and starfruit"
   return array.join(", ")
-elsif array.size > 3
+else array.size > 3
   last_index = array.size
   array[last_index - 1] = "and #{array{last_index - 1}"
   return array.join(", ")
